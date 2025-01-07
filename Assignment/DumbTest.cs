daf6826592664134a6a554a699f36ec3b901a320
LHT082024 <LHT082024@gmail.com>
2025-01-07 11:51:23 +0100

    testing stuff

--- Assignment/DumbTest.cs
+++ Assignment/DumbTest.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using C_ASSIGNMENT_BUILDER.Engine.AssignmentBuilder;
 
 namespace C_ASSIGNMENT_BUILDER.Assignment
 {
-    public class DumbTest()
+    public class DumbTest : AssignmentBase
     {
         public string dummy(string lol)
         {
             return lol;
+            throw new NotImplementedException();
         }
 
     }
