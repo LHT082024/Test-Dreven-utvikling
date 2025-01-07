51909ca74723218412662459225449dc19fe741e
LHT082024 <LHT082024@gmail.com>
2025-01-07 11:58:01 +0100

    made my own test

--- Assignment/DumbTest.cs
+++ Assignment/DumbTest.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using C_ASSIGNMENT_BUILDER.Engine.AssignmentBuilder;
+using Xunit;
 
 namespace C_ASSIGNMENT_BUILDER.Assignment
 {
     public class DumbTest : AssignmentBase
     {
-        public string dummy(string lol)
+        public string Dummy(string lol)
         {
             return lol;
             throw new NotImplementedException();
+        }
+
+
+        [Assignment(1)]
+        public void ReturnDummy()
+        {
+            string dummy = "im like so smart";
+            Assert.Equal(Dummy(dummy), dummy);
         }
 
     }
